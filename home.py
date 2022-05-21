33eaacc69aaada4dc5ba7428fbce0c17fac73cdd
Ngo Nguyen Hoang Dung <nnhoangdungdh@gmail.com>
2022-05-21 10:41:51 +0700

    auto

---
+++ home.py
@@ -0,0 +1,48 @@
+from sre_constants import IN
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(15,GPIO.IN)
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    GPIO.output(17, GPIO.LOW)
+    GPIO.output(18, GPIO.LOW)
+    return render_template('index.html')
+
+
+@app.route('/turnON', methods = ['POST', 'GET'])
+def turnON():
+    checkyel = request.form.get('checkyel')
+    checkblue = request.form.get('checkblue')
+    print(checkyel, checkblue)
+    if checkyel == 'on' and checkblue is None:
+        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(18, GPIO.LOW)
+        return render_template('turnOnYel.html')
+    if checkblue == 'on' and checkyel is None:
+        GPIO.output(17, GPIO.LOW)
+        GPIO.output(18, GPIO.HIGH)
+        return render_template('turnOnBlue.html')
+    if checkblue == 'on' and checkyel == 'on':
+        GPIO.output(17, GPIO.HIGH)
+        GPIO.output(18, GPIO.HIGH)
+        return render_template('turnOnBoth.html')
+    else:
+        GPIO.output(17, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
+        return render_template('index.html')
+
+
+@app.route('/turnONAuto', methods = ['POST', 'GET'])
+def turnOnAuto():
+    state_lamp = GPIO(15)
+    return render_template('turnOnAuto.html',state_lamp=state_lamp)
+    
+app.run(debug=True)
